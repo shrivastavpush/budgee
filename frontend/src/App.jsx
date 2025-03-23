65fe5529903cf72ff27f500a303830c967185e1e
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-03-23 19:17:24 +0530

    Project Setup + Front End Routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
+
+const Root = () => {
+    const isAuthenticated = true
+
+    return !isAuthenticated ?
+        <Navigate to='/login' />
+        :
+        <Navigate to='/dashboard' />
+
+}
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Root />} />
+                    <Route path='/login' exact element={<Login />} />
+                    <Route path='/signup' exact element={<SignUp />} />
+                    <Route path='/dashboard' exact element={<Home />} />
+                    <Route path='/income' exact element={<Income />} />
+                    <Route path='/expense' exact element={<Expense />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default App
