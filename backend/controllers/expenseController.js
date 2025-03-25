2d1965c9e9cd0b397d077a88c5fe60e8896d0f71
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-03-25 20:08:12 +0530

    added Expense amount handler.

---
+++ backend/controllers/expenseController.js
@@ -0,0 +1,91 @@
+const xlsx = require('xlsx')
+const Expense = require('../models/Expense')
+
+// add Expense
+exports.addExpense = async (req, res) => {
+    const userId = req.user.id
+    if (!userId) {
+        return res.status(401).json({ message: "Unauthorized, user not found" });
+    }
+
+    try {
+        const { icon, category, amount, date } = req.body
+
+        //checking for missing fields
+        if (!category || !amount || !date) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const newExpense = new Expense({
+            userId,
+            icon,
+            category,
+            amount,
+            date: new Date(date)
+        })
+
+        await newExpense.save()
+        res.status(200).json(newExpense)
+
+    } catch (error) {
+        // console.error("Error adding Expense:", error);
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+// get all Expense
+exports.getAllExpense = async (req, res) => {
+    const userId = req.user.id
+    if (!userId) {
+        return res.status(401).json({ message: "Unauthorized, user not found" });
+    }
+
+    try {
+        const expense = await Expense.find({ userId }).sort({ date: -1 })
+        res.json(expense)
+
+    } catch (error) {
+        // console.error("Error adding Expense:", error);
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+// delete Expense
+exports.deleteExpense = async (req, res) => {
+    try {
+        await Expense.findByIdAndDelete(req.params.id)
+        res.json({ message: "Expense deleted successfully" });
+    } catch (error) {
+        // console.error("Error adding Expense:", error);
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+// downlaod excel
+exports.downloadExpenseExcel = async (req, res) => {
+    const userId = req.user.id
+    if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized, user not found' });
+    }
+
+    try {
+        const expense = await Expense.find({ userId }).sort({ date: -1 })
+
+        //Preparing data for Excel
+        const data = expense.map((item) => ({
+            Category: item.category,
+            Amount: item.amount,
+            Date: item.date,
+        }))
+
+        const wb = xlsx.utils.book_new()
+        const ws = xlsx.utils.json_to_sheet(data)
+
+        xlsx.utils.book_append_sheet(wb, ws, 'Expense')
+        xlsx.writeFile(wb, 'expense_details.xlsx')
+        res.download('expense_details.xlsx')
+    } catch (error) {
+        // console.error("Error adding Expense:", error);
+        res.status(500).json({ message: "Server Error" })
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes')
 const incomeRoutes = require('./routes/incomeRoutes')
+const expenseRoutes = require('./routes/expenseRoutes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -24,6 +25,7 @@ connectDB()
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/income", incomeRoutes)
+app.use("/api/v1/expense", expenseRoutes)
 
 //Serving upload folder
 app.use('/uploads', express.static(path.join(__dirname, "uploads")))
