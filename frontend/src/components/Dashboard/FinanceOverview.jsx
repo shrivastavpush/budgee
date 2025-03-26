568c945885edee97e638336a3fe97b4d015adf42
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-03-27 00:22:35 +0530

    Dashboard: Added Custom PieChart

---
+++ frontend/src/components/Dashboard/FinanceOverview.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import CustomPieChart from '../Charts/CustomPieChart'
+
+const COLORS = ["#000000", "#22C55E", "#EF4444"]
+
+const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
+
+    const balanceData = [
+        { name: "Total Balance", amount: totalBalance },
+        { name: "Total Income", amount: totalIncome },
+        { name: "Total Expenses", amount: totalExpense },
+    ]
+
+    return (
+        <div className='card'>
+            <div className='flex items-center justify-between'>
+                <h5 className='text-lg'>Financial Overview</h5>
+            </div>
+
+            <CustomPieChart
+                data={balanceData}
+                label="Total Balance"
+                totalAmount={`₹${totalBalance}`}
+                colors={COLORS}
+                showTextAnchor
+            />
+        </div>
+    )
+}
+
+export default FinanceOverview
--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -11,6 +11,7 @@ import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu'
 import { IoMdCard } from 'react-icons/io'
 
 import RecentTransactions from '../../components/Dashboard/RecentTransactions'
+import FinanceOverview from '../../components/Dashboard/FinanceOverview'
 
 const Home = () => {
     useUserAuth()
@@ -73,6 +74,12 @@ const Home = () => {
                     <RecentTransactions
                         transactions={dashboardData?.recentTransactions}
                         onSeeMore={() => navigate('/expense')}
+                    />
+
+                    <FinanceOverview
+                        totalBalance={dashboardData?.totalBalance || 0}
+                        totalIncome={dashboardData?.totalIncome || 0}
+                        totalExpense={dashboardData?.totalExpense || 0}
                     />
                 </div>
             </div>
