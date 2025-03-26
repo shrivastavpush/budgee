07bc78f34748585e41a392e9fd8b580b9122f0d5
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-03-26 14:52:47 +0530

    Added Dashboard layout + Navbar Designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from './pages/Auth/SignUp'
 import Home from './pages/Dashboard/Home'
 import Income from './pages/Dashboard/Income'
 import Expense from './pages/Dashboard/Expense'
-import UserProvider from './context/userContext'
+import UserProvider from './context/UserContext'
 
 const Root = () => {
     const isAuthenticated = true
