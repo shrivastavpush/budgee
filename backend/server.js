a50720adce1fb92b926c3aa536c04ade09283335
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-12-18 11:06:46 +0530

    feat: initialize Express server with core middleware, routes, and error handling

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const dashboardRoutes = require('./routes/dashboardRoutes')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.set('trust proxy', 1);
 
 const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"];
 
